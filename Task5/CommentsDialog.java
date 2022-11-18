1824ac37b949faafe4a1039379c0a8ae0b2104a7
tianhui-shi <tianhui.shi@my.jcu.edu.au>
2022-11-18 22:59:06 +0800

    version 4

--- Task5/CommentsDialog.java
+++ Task5/CommentsDialog.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 public class CommentsDialog {
     public static void main(String[] args){
-        
+
         JOptionPane.showMessageDialog(null,
                 "Program comment are nonexecuting statements you add to a file for the purpose of documentation");
     }
